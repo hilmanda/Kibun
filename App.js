11f5d74a950dbc1be057fe32696d47944c8dc035
hilmanda <90018036+hilmanda@users.noreply.github.com>
2022-11-28 20:51:13 +0700

    Add dependencies

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Sharing from 'expo-sharing';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>BUAT APK NYA WOI :V</Text>
       <StatusBar style="auto" />
     </View>
   );
